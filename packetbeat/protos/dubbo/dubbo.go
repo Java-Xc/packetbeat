920b433deb83d951eafe53dfd7ecca413efff076
Java-Xc <394233079@qq.com>
2023-10-07 14:50:29 +0800

    更新

--- packetbeat/protos/dubbo/dubbo.go
+++ packetbeat/protos/dubbo/dubbo.go
@@ -18,10 +18,13 @@
 package dubbo
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"github.com/elastic/beats/v7/libbeat/common"
 	"github.com/elastic/beats/v7/packetbeat/procs"
 	"github.com/elastic/beats/v7/packetbeat/protos"
+	"github.com/elastic/beats/v7/packetbeat/protos/tcp"
 	conf "github.com/elastic/elastic-agent-libs/config"
 	"github.com/elastic/elastic-agent-libs/logp"
 	"time"
@@ -37,7 +40,6 @@ type dubboStream struct {
 	data []byte
 
 	parseOffset int
-	isClient    bool
 
 	message *dubboMessage
 }
@@ -46,6 +48,18 @@ type dubboMessage struct {
 	ts        time.Time
 	isRequest bool
 	tcpTuple  common.TCPTuple
+	// 魔数（2 字节）
+	magic uint16
+	// 标志（1 字节）
+	flag string
+	// 状态（1 字节）
+	status string
+	// 请求 ID（8 字节，大端序）
+	requestID uint64
+	// 数据长度（4 字节，大端序）
+	dataLen uint32
+	// 数据（不定长）
+	dubboData string
 }
 
 type dubboPlugin struct {
@@ -110,13 +124,79 @@ func (dubbo *dubboPlugin) ConnectionTimeout() time.Duration {
 
 func dubboMessageParser(s *dubboStream) (bool, bool) {
 
-	return true, false
+	// 读取Dubbo魔数（0xda, 0xbb）
+	magic := s.readBytes(2)
+	if !bytes.Equal(magic, []byte{0xda, 0xbb}) {
+		return false, false
+	}
+
+	// 读取Dubbo消息类型（请求或响应）
+	messageType := s.readByte()
+	isRequest := messageType == 0
+
+	// 读取请求/响应ID
+	requestID := s.readUint64()
+
+	// 如果是请求，解析服务名和方法名
+	if isRequest {
+		serviceName := s.readString()
+		methodName := s.readString()
+		// 解析请求参数
+		parameters := s.readBytes(len(s.data))
+		// 打印解析结果
+		fmt.Printf("Request ID: %d\nService Name: %s\nMethod Name: %s\nParameters: %s\n", requestID, serviceName, methodName, parameters)
+		return true, false // 表示成功解析请求
+
+	} else {
+		// 如果是响应，解析响应结果
+		// 响应状态，通常为0表示成功
+		status := s.readByte()
+		result := s.readBytes(len(s.data))
+		fmt.Printf("Request ID: %d\nStatus: %d\nResult: %s\n", requestID, status, result)
+		return false, true // 表示成功解析请求
+	}
+
+	return false, false
+}
+
+// 以下是辅助函数，用于读取不同类型的数据
+func (s *dubboStream) readByte() byte {
+	b := s.data[0]
+	s.data = s.data[1:]
+	return b
+}
+
+func (s *dubboStream) readBytes(length int) []byte {
+	bytes := s.data[:length]
+	s.data = s.data[length:]
+	return bytes
+}
+
+func (s *dubboStream) readUint64() uint64 {
+	data := s.readBytes(8)
+	return binary.BigEndian.Uint64(data)
+}
+
+func (s *dubboStream) readString() string {
+	length := int(s.readUint32())
+	return string(s.readBytes(length))
+}
+
+func (s *dubboStream) readUint32() uint32 {
+	data := s.readBytes(4)
+	return binary.BigEndian.Uint32(data)
 }
 
 func (stream *dubboStream) prepareForNewMessage() {
 	stream.data = stream.data[stream.parseOffset:]
 	stream.parseOffset = 0
 	stream.message = nil
+}
+
+// 重置解析状态
+func (s *dubboStream) reset() {
+	s.parseOffset = 0
+	s.message = nil
 }
 
 // 处理空包丢包
@@ -143,15 +223,11 @@ func (dubbo *dubboPlugin) ReceivedFin(tcptuple *common.TCPTuple, dir uint8,
 func (dubbo *dubboPlugin) Parse(pkt *protos.Packet, tcptuple *common.TCPTuple,
 	dir uint8, private protos.ProtocolData,
 ) protos.ProtocolData {
-
 	// 解析 Dubbo 数据包的逻辑
 	// 在这里处理 Dubbo 协议的解析逻辑
 	fmt.Println("解析 Dubbo 数据包:", pkt.Payload)
-	logp.Info(string(pkt.Payload))
 
 	priv := dubboPrivateData{}
-
-	/*priv := dubboPrivateData{}
 	if private != nil {
 		var ok bool
 		priv, ok = private.(dubboPrivateData)
@@ -161,10 +237,7 @@ func (dubbo *dubboPlugin) Parse(pkt *protos.Packet, tcptuple *common.TCPTuple,
 	}
 
 	if priv.data[dir] == nil {
-		dstPort := tcptuple.DstPort
-		if dir == tcp.TCPDirectionReverse {
-			dstPort = tcptuple.SrcPort
-		}
+		//客户端到服务器端的请求
 		priv.data[dir] = &dubboStream{
 			data:    pkt.Payload,
 			message: &dubboMessage{ts: pkt.Ts},
@@ -200,6 +273,21 @@ func (dubbo *dubboPlugin) Parse(pkt *protos.Packet, tcptuple *common.TCPTuple,
 			// wait for more data
 			break
 		}
-	}*/
+	}
 	return priv
+}
+
+// Called when the parser has identified a full message.
+func (dubbo *dubboPlugin) messageComplete(tcptuple *common.TCPTuple, dir uint8, stream *dubboStream) {
+	// and reset message
+	stream.prepareForNewMessage()
+}
+
+func (dubbo *dubboPlugin) isServerPort(port uint16) bool {
+	for _, sPort := range dubbo.ports {
+		if uint16(sPort) == port {
+			return true
+		}
+	}
+	return false
 }
