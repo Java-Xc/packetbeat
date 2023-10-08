a8e1dde8c7e34d72037c0ead9a8dde7f99ec2cac
Java-Xc <394233079@qq.com>
2023-10-08 14:27:06 +0800

    更新

--- packetbeat/protos/dubbo/dubbo.go
+++ packetbeat/protos/dubbo/dubbo.go
@@ -233,23 +233,8 @@ func (dubbo *dubboPlugin) Parse(pkt *protos.Packet, tcptuple *common.TCPTuple,
 	// Assuming Dubbo header starts at offset 0 in the packet data
 	dubboHeader := pkt.Payload[:16] // Extracting first 16 bytes as Dubbo header
 
-	// Extracting the first byte of Dubbo header
-	thirdByte := dubboHeader[2]
-
-	// Extracting the Req/Res flag (1st bit of the first byte)
-	reqResFlag := (thirdByte & 0x80) >> 7
-	fmt.Println("请求标识为:", reqResFlag)
-
-	//判断是否为dubbo协议（通过魔数）
-	/*ok, remainingData := isDubbo(pkt.Payload)
-	if ok {
-		fmt.Println("解析 Dubbo 数据包1:", remainingData)
-		payload := string(remainingData)
-		lines := strings.Split(payload, "\n")
-		for i, line := range lines {
-			fmt.Printf("Line %d: %s\n", i+1, line)
-		}
-	}*/
+	isDubbo(dubboHeader)
+	isRequest(dubboHeader)
 
 	// 解析 Dubbo 协议消息
 	/*messageType, remainingData := parseMessageType(pkt.Payload)
@@ -318,130 +303,6 @@ func (dubbo *dubboPlugin) Parse(pkt *protos.Packet, tcptuple *common.TCPTuple,
 		return priv*/
 }
 
-// 判断是否为dubbo协议（以魔数判断）
-func isDubbo(payload []byte) (bool, []byte) {
-	// 判断负载长度是否大于等于4个字节（Dubbo 魔数长度）
-	if len(payload) < 2 {
-		fmt.Println("Payload length is less than 2 bytes, unable to read Dubbo magic number")
-		return false, payload
-	}
-	// 读取前四个字节作为 Dubbo 魔数
-	dubboMagic := payload[:2]
-	// 判断 Dubbo 魔数是否匹配
-	if !bytes.Equal(dubboMagic, []byte{0xda, 0xbb}) {
-		fmt.Printf("Dubbo magic number not found. Got: %x\n", dubboMagic)
-		return false, payload
-	}
-	remainingData := payload[2:]
-	return true, remainingData
-}
-
-// 解析 Dubbo 消息类型
-func parseMessageType(payload []byte) (int, []byte) {
-	if len(payload) < 2 {
-		// 数据包长度不足以解析消息类型
-		return 0, payload
-	}
-
-	// Dubbo 协议中，消息类型通常是一个16位的整数
-	messageType := binary.BigEndian.Uint16(payload)
-	remainingData := payload[2:]
-
-	return int(messageType), remainingData
-}
-
-// 解析 Dubbo 请求ID
-func parseRequestID(payload []byte) (int64, []byte) {
-	if len(payload) < 8 {
-		// 数据包长度不足以解析请求ID
-		return 0, payload
-	}
-
-	// Dubbo 协议中，请求ID通常是一个64位的整数，使用大端字节序存储
-	requestID := int64(binary.BigEndian.Uint64(payload[:8]))
-	remainingData := payload[8:]
-
-	return requestID, remainingData
-}
-
-// 解析 Dubbo 服务名
-func parseServiceName(payload []byte) (string, []byte) {
-	// 假设 Dubbo 协议的服务名以 0x2f 字节作为分隔符
-	separator := byte(0x2f)
-
-	// 查找第一个分隔符的位置
-	separatorIndex := bytes.IndexByte(payload, separator)
-	if separatorIndex == -1 {
-		// 没有找到分隔符，无法解析服务名
-		return "", payload
-	}
-
-	// 提取服务名部分
-	serviceName := string(payload[:separatorIndex])
-	remainingData := payload[separatorIndex+1:]
-
-	return serviceName, remainingData
-}
-
-// 解析 Dubbo 方法名
-func parseMethodName(payload []byte) (string, []byte) {
-	// 假设 Dubbo 协议的方法名以 0x2e 字节作为分隔符
-	separator := byte(0x2e)
-
-	// 查找第一个分隔符的位置
-	separatorIndex := bytes.IndexByte(payload, separator)
-	if separatorIndex == -1 {
-		// 没有找到分隔符，无法解析方法名
-		return "", payload
-	}
-
-	// 提取方法名部分
-	methodName := string(payload[:separatorIndex])
-	remainingData := payload[separatorIndex+1:]
-
-	return methodName, remainingData
-}
-
-// 解析 Dubbo 参数
-func parseParameters(payload []byte) (map[string]interface{}, []byte) {
-	parameters := make(map[string]interface{})
-
-	// 解析参数的逻辑需要根据 Dubbo 协议规范和实际情况来实现
-	// 这里演示解析字符串、整数和布尔值参数的示例
-
-	for len(payload) > 0 {
-		// 参数类型标志位
-		paramType := payload[0]
-		payload = payload[1:]
-
-		switch paramType {
-		case 'S':
-			// 字符串参数
-			strLen := binary.BigEndian.Uint16(payload[:2])
-			payload = payload[2:]
-			paramValue := string(payload[:strLen])
-			payload = payload[strLen:]
-			parameters["stringParam"] = paramValue
-		case 'I':
-			// 整数参数
-			paramValue := int(binary.BigEndian.Uint32(payload[:4]))
-			payload = payload[4:]
-			parameters["intParam"] = paramValue
-		case 'B':
-			// 布尔值参数
-			paramValue := payload[0] != 0
-			payload = payload[1:]
-			parameters["boolParam"] = paramValue
-		default:
-			// 未知参数类型，可以根据需求处理
-			// 这里可以记录日志或进行其他处理
-			// 或者根据 Dubbo 协议规范处理其他参数类型
-		}
-	}
-
-	return parameters, payload
-}
-
 // Called when the parser has identified a full message.
 func (dubbo *dubboPlugin) messageComplete(tcptuple *common.TCPTuple, dir uint8, stream *dubboStream) {
 	// and reset message
@@ -455,4 +316,32 @@ func (dubbo *dubboPlugin) isServerPort(port uint16) bool {
 		}
 	}
 	return false
+}
+
+// 判断是否为dubbo协议（以魔数判断）
+func isDubbo(dubboHeader []byte) bool {
+	// 判断负载长度是否大于等于4个字节（Dubbo 魔数长度）
+	if len(dubboHeader) < 2 {
+		fmt.Println("dubboHeader length is less than 2 bytes, unable to read Dubbo magic number")
+		return false
+	}
+	// 读取前2个字节作为 Dubbo 魔数
+	dubboMagic := dubboHeader[:2]
+	fmt.Println("dubboMagic:", dubboMagic)
+	// 判断 Dubbo 魔数是否匹配
+	if !bytes.Equal(dubboMagic, []byte{0xda, 0xbb}) {
+		fmt.Printf("Dubbo magic number not found. Got: %x\n", dubboMagic)
+		return false
+	}
+	return true
+}
+
+// 判断是否为请求/响应
+func isRequest(dubboHeader []byte) bool {
+	// Extracting the 3 byte of Dubbo header
+	thirdByte := dubboHeader[2]
+	fmt.Println("thirdByte:", thirdByte)
+	reqResFlag := (thirdByte & 0x80) >> 7
+	//请求：1，响应：0。
+	return reqResFlag == 0
 }
